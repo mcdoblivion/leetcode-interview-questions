6e0a0865bbc6b57f43709016a7a8dc27388ed645
mcd_oblivion <mcd.oblivion@gmail.com>
2021-08-17 00:36:44 +0700

    First Unique Character in a String

---
+++ Strings/FirstUniqueCharacterInAString.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function (s) {
+    for (let i = 0; i < s.length; i++) {
+        if (s.indexOf(s[i]) === i && s.indexOf(s[i], i + 1) === -1) return i
+    }
+    return -1
+}
